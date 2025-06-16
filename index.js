beef56d40e0cf6ee85e47fb7cb9c17d2b7afddf2
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 11:19:39 -0700

    [Backend] Add /hello-world endpoint

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
     res.send("Welcome to my app!");
 });
+
+app.get("/hello-world", (req, res) => {
+    res.send("Hello, World!")
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
