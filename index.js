556de4e8e9a6a6d71aa159432df1d289e4ebd349
Javier Carrillo Zamora <javiercarrillozamora@gmail.com>
2025-06-16 11:09:10 -0700

    [Backend] Setup root path

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Welcome to my app!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
